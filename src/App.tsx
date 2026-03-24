d1b2005793dd6ffa24fe3d70eb07eac4582b8c8a
FedrosFieros <109699750+FedrosFieros@users.noreply.github.com>
2026-03-24 09:28:03 +0000

    a

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,12 @@ const sections = [
       "Use the left navbar or Next/Back buttons to explore."
     ],   presenterPosition: "center-right"
   },
-  { title: "Code-L Phase 1 ", phase: "cave", text: ["Users first experience the mythos and alternative world of Code-L, through inside a 3D cave, accessed through a website."],     image: "/cave1.png",   presenterPosition: "top-right" },
-  { title: "The Cave", phase: "cave", text: ["Once a player is inside, they can control an avatar.", "But they are not alone. Other online participants will be there in the same room.", "Each player has the same looking avatar but different usernames and a chat bubble to chat to all."], image: "/cave-avatars.png"  },
+  { title: "Code-L Phase 1 ", phase: "cave", text: ["Users first experience the mythos and alternative 3D ElWorld,  inside a 3D cave, accessed through a website el.world."],     image: "/cave1.png",   presenterPosition: "top-right" },
+  { title: "The Cave", phase: "cave", text: ["Once a player is inside, they can control an avatar.", "But they are not alone. Other online participants will be there in the same room.", "Each player has the same looking avatar but different usernames and a chat bubble to chat to all. They cannot create an account yet, so everytime they spawn as a new character."], image: "/cave-avatars.png"  },
   { title: "GamePlay", phase: "cave", text: ["Players can view screen from first perspective and move their characters to interact with environment and other online players..", ], image: "/firstperspective.png"  },
   { title: "Backstory - Ahriman's plan", phase: "cave", text: ["These caves have been designed by Ahriman to trap humans inside systems which they cannot escape. Deceiving them with a false idea of reality, he holds them prisoners."], image: "/Ahriman.png"  },
 { title: "The Puppeteer", phase: "cave", text: ["controlled by a person in ElCode from backend, puppeteer speaks of safety inside the cave and chaos outside..", "She maintains order and calms the participants."] ,image: "/cave-puppeteer.png"},
-  { title: "The Blue Device", phase: "cave", text: ["A live feed of events posted by puppeteers from backend explaining events happening in mythos Code-L, from an Ahriman perspective."],image: "/cave-bluedevice.png" },
+  { title: "The Blue Device", phase: "cave", text: ["A device every player has with live feed of events posted by puppeteers from backend explaining events happening in mythos Code-L, from an Ahriman perspective. ","This gives opportunity for live participants to talk about them. Room for discussion."],image: "/cave-bluedevice.png" },
   { title: "The Wolves Ritual", phase: "cave", text: ["At certain moments the puppeteer may invite prisoners to follow her toward a dark dungeon located at the back of the cave.",
 "Participants who follow enter a ritual space.",
 "Within the dungeon a disturbing ceremony unfolds:",
@@ -71,13 +71,14 @@ const sections = [
   
   { title: "The  Boat", phase: "escape", text: ["Players who join Ellys, board on the boat and sail to island of Pandora, arriving on ELSPARK."] , image:"/boat.png"},
 
-  { title: "ELSPARK 3D", phase: "elspark", text: ["A new decentralised world emerges on the island of Pandora.", "An extension of elspark.online, but on a 3D world with every user having their own avatar"], image:"/ELSPARK.png" },
-  { title: "ELSPARK Theatre", phase: "elspark", text: ["They are imemdiately guided to ELSPARK Theatre where, Ellys takes on the stage.","when the lights dim and curtains open, he reveals a screen"], image:"/ELSPARKTheatre.png" },
+  { title: "ELSPARK 3D", phase: "elspark", text: ["A new decentralised world emerges on the island of Pandora.", "An extension of elspark.online, but on a 3D world with every user now having the option to create their own avatar"], image:"/ELSPARK.png" },
+  { title: "ELSPARK Theatre", phase: "elspark", text: ["As soon as they arrive at ELSPARK, they are imemdiately guided to ELSPARK Theatre where, Ellys takes on the stage.","when the lights dim and curtains open, he reveals a screen"], image:"/ELSPARKTheatre.png" },
   { title: "ELSPARK TV", phase: "elspark", text: ["The screen is ELSPARK TV, a digital platform broadcasting live from Apothiki, our first edition of ELCA -  a live theatrical performance we execute in physical space."], image:"/ELSPARKTV.png" },
-
-  { title: "ELSPARK NEWS", phase: "elspark", text: ["ELTV is our media production company and we connect creatives across Europe to produce independent shows, like ELSPARK NEWS, which are broadcasted onto ELSPARK TV. A curated channel only for our original shows.."],image:"/ELSPARKNEWS.png" },
-  { title: "What happens after? Phase 3", phase: "elspark", text: ["ELSPARK continues being live in 3D, welcoming new audiences away from Ahriman's corrupted social media spaces.",] },
-  { title: "ELTV", phase: "elspark", text: ["We continue producing more shows, broadcasting through ELSPARK TV compensating creators with profits.",] },
+  { title: "ELCA", phase: "elspark", text: ["A live theatrical performance with the following schedule:","1) Monarch of my Body","2)ELSPARK NEWS","3)live performances with skits or music from local performers on city we host it everytime"], image:"/ELSPARKTheatre.png" },
+  
+  { title: "ELSPARK NEWS", phase: "elspark", text: ["A sitirical depiction of news in the world, through the mythos of Code-L but with human actors. We use archetypes to comment and satire to reflect.", "ELTV is our media production company and we connect creatives across Europe to produce independent shows, like ELSPARK NEWS, which are broadcasted onto ELSPARK TV. A curated channel only for our original shows.."],image:"/ELSPARKNEWS.png" },
+  { title: "What happens after? Phase 3", phase: "elspark", text: ["ELWORD continues being accesible as a 3D world, and ELSPARK welcomes new audiences away from Ahriman's corrupted social media spaces.","New accounts always start in the cave, but ones who create an account on ELSPARK can always spawn from there."] },
+  { title: "ELTV", phase: "elspark", text: ["We continue producing more shows, broadcasting through ELSPARK TV compensating creators with direct profits or funding from ElCode.",] },
   { title: "Elly's Code", phase: "elspark", text: ["Elly's debut studio album is available to be experienced through an interactive website through which users can observe the prequel to the events on June 1st..","split in two ACTS, ACTI follows story of a caterpillar transforming to a white moth, while ACT II follows story of Ellys breaking out of the cave for the first time and his journey to find ELSPARK.Then on the 6th track  he comes back and invites other prisoners to join him on ELSPARK."] },
 
 
